e1cf4c337b4f71cc5e912044694e5855a6cd1c55
dani <dani@DESKTOP-PV10JA8>
2024-07-21 12:07:59 +0200

    new

--- gzBackend/core/settings/base.py
+++ gzBackend/core/settings/base.py
@@ -90,12 +90,24 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+            "default": {
+                "ENGINE" : "django.db.backends.postgresql",
+                "NAME": "gamingzone",
+                "USER": "divatech" ,
+                "PASSWORD": "divatech",
+                "HOST":"localhost",
+                "PORT": 5432, 
+                    }
+            }
+
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 # Password validation
