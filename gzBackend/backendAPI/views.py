b6ef56382cf15bb6f16b9caf0300aac631694e58
dani <95laidi@gmail.com>
2024-07-21 22:00:11 +0100

    cach

--- gzBackend/backendAPI/views.py
+++ gzBackend/backendAPI/views.py
@@ -54,9 +54,36 @@ class Produit(viewsets.ModelViewSet):
                     "new": prod.new,
                     "sections": prod.get_sections()
                 })
-                cache.set(key,products_data,timeout=15*60)  #secand 
+                cache.set(key,products_data,timeout=15*60)  #15 MN 
         return  Response ({'products': products_data, 'source': 'PSQL'},status=status.HTTP_200_OK)
-    
+    def retrieve(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        key = f"product_{pk}"
+        product_data = cache.get(key)
+        
+        if product_data:
+            return Response({'product': product_data, 'source': 'cache'}, status=status.HTTP_200_OK)
+        
+        product = self.get_object()
+        product_data = {
+            "id": product.id,
+            "name": product.designation,
+            "description": product.description,
+            "price": product.price,
+            "images": product.get_image(),
+            "stock": product.quantity,
+            "deal": product.promo,
+            "deal price": product.price_promo,
+            "category": product.category.component if product.category else '',
+            "filter": product.get_filters(),
+            "config": product.config,
+            "new": product.new,
+            "sections": product.get_sections()
+        }
+        
+        cache.set(key, product_data, timeout=15*60)  # 15 MN
+        
+        return Response({'product': product_data, 'source': 'PSQL'}, status=status.HTTP_200_OK)    
 
 
 
